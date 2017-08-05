4cd1ee24307ca47e770104752ff1334ecb844ac6
jayms8462 <jayms.portune@gmail.com>
2017-08-05 10:07:09 -0700

    completed the last 2 ToDos

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -20,11 +20,14 @@ Article.prototype.toHtml = function() {
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
   //   For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
+  var source = $('#meaningful').html();
+  var template = Handlebars.compile(source);
+
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
   this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
   // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
-
+  return template(this);
 };
 
 rawData.sort(function(a,b) {
